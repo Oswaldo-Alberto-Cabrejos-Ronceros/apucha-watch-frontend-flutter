2d1a612e4c386398174c293b3bbc5f064df25e61
Oswaldo <U22209742@utp.edu.pe>
2025-09-23 21:50:47 -0500

    feat:Inclusion de api key para goggle maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:apucha_watch_movil/core/theme/app_theme.dart';
 import 'package:apucha_watch_movil/screens/home_user/home_user_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  //for env variables
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
