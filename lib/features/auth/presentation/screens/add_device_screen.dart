0b04cd482e0765e076708f5cd917bc8b4c496527
Oswaldo <U22209742@utp.edu.pe>
2025-09-27 12:49:44 -0500

    feat:Desarrollo de pantalla add device

---
+++ lib/features/auth/presentation/screens/add_device_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AddDeviceScreen extends StatefulWidget {
+  const AddDeviceScreen({super.key});
+
+  @override
+  State<AddDeviceScreen> createState() => _AddDeviceScreenState();
+}
+
+class _AddDeviceScreenState extends State<AddDeviceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 12,
+              children: [
+                Text('Vincular reloj', style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: Theme.of(context).colorScheme.primary)),
+                Text(
+                  'Ingrese el codigo del reloj',
+                  style: TextStyle(fontSize: 16),
+                ),
+                //textfield email
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Codigo del reloj',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                FilledButton(onPressed: () {}, child: Text('Vincular')),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
