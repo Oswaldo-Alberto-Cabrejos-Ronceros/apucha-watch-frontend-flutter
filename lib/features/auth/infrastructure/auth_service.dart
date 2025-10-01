09a64ab2ea3d835d3363d2c6262c031a95ba411a
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-10-01 16:51:57 -0500

    feat: desarrollo de metodos register y logout en auth service

--- lib/features/auth/infrastructure/auth_service.dart
+++ lib/features/auth/infrastructure/auth_service.dart
@@ -13,7 +13,7 @@ class AuthService{
           "password": password
         }
       );
-      if(response.status ==200){
+      if(response.statusCode ==200){
         final authResponse = AuthResponse.fromJson(response.data);
       }
     } on DioException catch (e){
@@ -21,5 +21,20 @@ class AuthService{
     }
     return null;
   }
-  
+
+  //for register
+  Future<bool> register(registerRequest: RegisterRequest) async {
+    try{
+      final response = await apiClient.post('auth/register', data: registerRequest.toJson());
+      return reponse.statusCode == 201;
+    } on DioException catch (e){
+      print("Error en register: ${e.response?.data ?? e.message}");
+      return false;
+    }
+  }
+
+  Future<void> logout ()async{
+    apiClient.clearToken();
+  }
+
 }
