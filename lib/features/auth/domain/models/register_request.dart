dce9e34a04feee846d250b48a96bbf9d1bea85bc
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-10-01 13:54:56 -0500

    feat: desarrollo de RegisterRequest

---
+++ lib/features/auth/domain/models/register_request.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'register_request.g.dart';
+
+@JsonSerializable()
+class RegisterRequest{
+  final String email;
+  final String password;
+  final String name;
+  final String lastname;
+  RegisterRequest({
+    required this.email,
+    required this.password,
+    required this.name,
+    required this.lastname,
+  })
+
+  factory RegisterRequest.fromJson(Map<String, dynamic> json)=> _$RegisterRequestFromJson(json);
+  Map<String, dynamic> toJson() =>_$RegisterRequestToJson(this);
+
+}
