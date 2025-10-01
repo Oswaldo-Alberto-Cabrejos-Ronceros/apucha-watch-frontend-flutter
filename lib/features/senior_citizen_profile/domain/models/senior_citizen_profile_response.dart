c85134d7ae3d0b279eab603cbd27e5229e73ab87
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-10-01 17:23:03 -0500

    feat:Creacion de modelos seniorCitizenProfileResponse y DeviceMinimunResponse

---
+++ lib/features/senior_citizen_profile/domain/models/senior_citizen_profile_response.dart
@@ -0,0 +1,28 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'senior_citizen_profile_response.g.dart';
+
+@JsonSerializable()
+class SeniorCitizenProfileResponse{
+  final int id;
+  final String name;
+  final String lastname;
+  final DateTime birthdate;
+  final DeviceMininumResponse device;
+  final DateTime createAt;
+  final DateTime? updateAt;
+  final DateTime? deleteAt;
+  
+  SeniorCitizenProfileResponse({
+    required this.id;
+    required this.name;
+    required this.lastname;
+    required this.birthdate;
+    required this.device;
+    required this.createAt;
+    this.updateAt;
+    this.deleteAt;
+  })
+
+   factory SeniorCitizenProfileResponse.fromJson(Map<String, dynamic> json)=> _$SeniorCitizenProfileResponseFromJson(json);
+  Map<String, dynamic> toJson() =>_$SeniorCitizenProfileResponseToJson(this);
+}
