48230e4af8b451a1881602ddbe256bcecb65743f
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-10-02 19:53:34 -0500

    feat: Consumo de servicio auth en register_screen

--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -22,34 +22,37 @@ class _RegisterScreenState extends State<RegisterScreen> {
   bool _loading = false;
 
   //metodo que inicia sesion
-  Future<void> _register() async{
-        setState(() {
+  Future<void> _register() async {
+    setState(() {
       _loading = true;
       _errorMessague = null;
     });
-    try{
+    try {
       //construimos un RegisterRequest
-      final registerRequest = RegisterRequest(email: _emailController.text,
-       password: _passwordController.text, name: _nameController.text, 
-       lastname: _lastnameController.text);
+      final registerRequest = RegisterRequest(
+        email: _emailController.text,
+        password: _passwordController.text,
+        name: _nameController.text,
+        lastname: _lastnameController.text,
+      );
 
-       final result = await widget.authService.register(registerRequest);
-       if(result){
-             //if exist
+      final result = await widget.authService.register(registerRequest);
+      if (result) {
+        //if exist
         if (!mounted) return;
         //por mientras despues poner pagina de espera de confimacion de correo
         Navigator.pushReplacementNamed(context, "/home");
-       } else{
-               setState(() {
+      } else {
+        setState(() {
           _errorMessague = 'Error al registar usuario';
         });
-       }
-    } catch (e){
-            setState(() {
+      }
+    } catch (e) {
+      setState(() {
         _errorMessague = 'Error inesperado $e';
       });
-    } finally{
-            setState(() {
+    } finally {
+      setState(() {
         _loading = false;
       });
     }
@@ -57,60 +60,84 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: Padding(padding: EdgeInsets.all(20),
-        child: Center(child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 12,
-          children: [
-            //title
-            Text('Apucha Watch', style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: Theme.of(context).colorScheme.primary)),
-            Text('Registro', style: TextStyle(fontSize: 16)),
-            //textfield email
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Correo electrónico',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            //textfield password
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            //textfield confirm password
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Confirmar Contraseña',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Nombres',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Apellidos',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            FilledButton(onPressed: (){}, child: Text('Registrarse')),
-            Center(child: Text('¿Ya tienes una cuenta?')),
-            TextButton(onPressed: (){
-              Navigator.pushNamed(context, '/login');
-            }, child: Text('Iniciar sesión'))
-          ],
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 12,
+              children: [
+                //title
+                Text(
+                  'Apucha Watch',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                Text('Registro', style: TextStyle(fontSize: 16)),
+                //textfield email
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Correo electrónico',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                //textfield password
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Contraseña',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+                //textfield confirm password
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Confirmar Contraseña',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nombres',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Apellidos',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                if (_loading) const CircularProgressIndicator(),
+                //error
+                if (_errorMessague != null)
+                  Text(
+                    _errorMessague ?? '',
+                    style: const TextStyle(color: Colors.red),
+                  ),
 
-        ),),
+                FilledButton(
+                  onPressed: _loading ? null : _register,
+                  child: Text('Registrarse'),
+                ),
+                Center(child: Text('¿Ya tienes una cuenta?')),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  child: Text('Iniciar sesión'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      
-    ),);
+    );
   }
 }
